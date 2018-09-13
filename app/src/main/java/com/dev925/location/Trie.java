13ee59f272e4b475c013a4824eef9707d8d1ff75
Robert Gross <mrgross17@gmail.com>
2018-09-13 08:30:25 -0400

    Removed unneeded file and tested file reading

--- app/src/main/java/com/dev925/location/Trie.java
+++
@@ -1,5 +0,0 @@
-package com.dev925.location;
-
-public class Trie {
-
-}
--- app/src/test/java/com/dev925/location/ExampleUnitTest.java
+++ app/src/test/java/com/dev925/location/ExampleUnitTest.java
@@ -3,6 +3,8 @@ package com.dev925.location;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 /**
@@ -12,12 +14,37 @@ import java.util.List;
  */
 public class ExampleUnitTest {
     @Test
-    public void canBuildTry() {
+    public void attemptToBuildTrie() {
         TrieNode root = new TrieNode();
         root.insert("car");
         root.insert("cat");
         List<String> carWords = root.query("car");
-        List<String> caWords = root.query("ca");
         Assert.assertEquals(carWords.size(), 1);
     }
+    @Test
+    public void attemptToQueryTrie() {
+        TrieNode root = new TrieNode();
+        root.insert("car");
+        root.insert("cat");
+        List<String> carWords = root.query("car");
+        Assert.assertEquals(carWords.size(), 1);
+
+        List<String> caWords = root.query("ca");
+        Assert.assertEquals(caWords.size(), 2);
+    }
+
+
+    @Test
+    public void attemptToReadFile() throws IOException {
+        ClassLoader classLoader = getClass().getClassLoader();
+        InputStream is = classLoader.getResourceAsStream("cities.json");
+        int size = is.available();
+        byte[] buffer = new byte[size];
+        is.read(buffer);
+        is.close();
+        String jsonAsString = new String(buffer, "UTF-8");
+        Assert.assertTrue(jsonAsString != null && jsonAsString.length() > 0);
+    }
+
+
 }
